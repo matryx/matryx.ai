328b70f635a0632369a7b56ee7b44e4d7a4c4cbb
Dominique Liau <deejay.whitegrass@gmail.com>
2017-08-10 10:57:51 -0600

    style faq

--- src/App.vue
+++ src/App.vue
@@ -26,10 +26,26 @@ body {
     margin: 0 auto;
 }
 
-#app {
-    margin: 0;
-    padding: 0;
-    height: 100%;
-    width: 100%;
+header {
+  background-color: $deep-black;
+  height:65px;
+  width:100%;
+
+  a {
+    text-decoration: none;
+    color: $white;
+    padding-right: 20px;
+  }
 }
+
+#app {
+  display: -webkit-box;display: -ms-flexbox;display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+
 </style>
--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,9 +1,16 @@
 <template>
-<div class="body-content">
+<div class="body-content content-container--large">
+  <h1>Frequently Asked Questions</h1>
   <aside class="faq-menu">
     <ul class="faq-menu__list">
-      <li class="faq-menu__list__list-item"><a href="faq-section-1">FAQ menu item 1</a></li>
-      <li class="faq-menu__list__list-item"><a href="faq-section-2">FAQ menu item 1</a></li>
+      <li class="faq-menu__list__list-item" @click.prevent="showAccordion(one)">
+        <a href="faq-section-1" >FAQ menu item 1</a>
+      </li>
+      <li class="faq-menu__list__list-item" @click.prevent="showAccordion(two)">
+        <a href="faq-section-2">FAQ menu item 1</a>
+      </li>
+
+      <router-link to="/faq/general">General Inquiries</router-link>
       <!-- <li class="faq-menu__list__list-item"><a href="faq-section-3"></a>3</li>
         <li class="faq-menu__list__list-item"><a href="faq-section-4"></a>4</li>
         <li class="faq-menu__list__list-item"><a href="faq-section-5"></a>5</li>
@@ -14,10 +21,11 @@
         <li class="faq-menu__list__list-item"><a href="faq-section-10"></a>10</li> -->
     </ul>
   </aside>
-  <section class="faq">
-    <div id="help1" class="faq__accordion">
+  <section class="faq content-container--medium">
+    <router-view></router-view>
+    <!-- <div id="help1" class="faq__accordion" v-show="accordion">
       <h1>FAQ Section 1</h1>
-      <b-btn class="faq__accordion__btn" block v-b-toggle.help1-accordion-1 variant="primary">Section 1 Question 1</b-btn>
+      <b-btn class="faq__accordion__btn" block v-b-toggle.help1-accordion-1 >Section 1 Question 1</b-btn>
       <b-collapse id="help1-accordion-1" accordion="help1-accordion">
         <b-card class="faq__accordion__card">
           Section 1 Answer 1
@@ -30,7 +38,7 @@
         </b-card>
       </b-collapse>
     </div>
-    <div id="help2" class="faq__accordion">
+    <div id="help2" class="faq__accordion" v-show="accordion">
       <h1>FAQ Section 2</h1>
       <b-btn class="faq__accordion__btn" block v-b-toggle.faq-1-accordion-2>Section 2 Question 1</b-btn>
       <b-collapse id="faq-1-accordion-2" accordion="faq-1-accordion">
@@ -38,74 +46,100 @@
           Section 2 Answer 2
         </b-card>
       </b-collapse>
-    </div>
+    </div> -->
   </section>
 </div>
 </template>
 
 <style lang="scss">
-@import '../assets/css/styles.scss';
+  @import '../assets/css/styles.scss';
 
-.body-content {
-  display: -webkit-box;display: -ms-flexbox;display: flex;
-  width: 100%;
-  height: 100%;
-  border: 1px solid red;
-}
-.faq-menu {
-  flex: 0 0 200px;
-  width: 300px;
-  border: 1px solid $deep-black;
-  display: -webkit-box;display: -ms-flexbox;display: flex;
+  .body-content {
+    display: -webkit-box;display: -ms-flexbox;display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    border: 1px solid red;
 
-  &__list {
-    margin-top: 100px;
-
-    &__list-item {
-      list-style: none;
-      line-height: 24px;
-
-      a {
-        text-decoration: none;
-        font-size: 14px;
-      }
+    > h1 {
+      flex: 0 0 100%;
+      align-self: flex-end;
+      text-align: center;
     }
   }
-}
-.faq {
-  flex: 1 1 900px;
-
-  display: -webkit-box;display: -ms-flexbox;display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-  border: 1px solid $deep-black;
-  padding: 20px;
-
-  &__accordion {
+  .faq-menu {
+    flex: 0 0 220px;
+    width: 300px;
+    border: 1px solid $deep-black;
+    margin: 15px;
     display: -webkit-box;display: -ms-flexbox;display: flex;
     flex-direction: column;
-    margin-bottom: 40px;
+    justify-content: space-between;
 
-    &__card {
-      border: 1px solid $deep-black;
-    }
+    &__list {
+      margin: 0;
+      padding: 0;
 
-    &__btn {
-      margin: 2px 0 0;
-      background-color: $midnight-tint-2;
-      border: none;
-      border-bottom: 1px solid transparent;
+      &__list-item {
+        list-style: none;
+        line-height: 24px;
 
-      &:active, &:focus {
-        outline: none;
+        a {
+          text-decoration: none;
+          font-size: 14px;
+        }
       }
     }
   }
-}
+  .faq {
+    flex: 1 1 0;
+
+    display: -webkit-box;display: -ms-flexbox;display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    border: 1px solid $deep-black;
+    padding: 20px;
+    margin:15px;
+
+    &__accordion {
+      display: -webkit-box;display: -ms-flexbox;display: flex;
+      flex-direction: column;
+      max-width: 800px;
+      margin-bottom: 40px;
+
+      &__card {
+        border: 1px solid $deep-black;
+      }
+
+      &__btn {
+        margin: 2px 0 0;
+        background-color: $midnight-tint-2;
+        border: none;
+        border-bottom: 1px solid transparent;
+
+        &:active, &:focus {
+          outline: none;
+        }
+      }
+    }
+  }
 </style>
 
 <script>
 export default {
-  name: 'FAQ'
+  name: 'FAQ',
+  data () {
+    return {
+      accordion: {
+        one: true,
+        two: false
+      }
+    }
+  },
+  methods: {
+    showAccordion: function (cardNum) {
+      console.log(cardNum)
+      this.accordion[cardNum] = true
+    }
+  }
 }
 </script>
