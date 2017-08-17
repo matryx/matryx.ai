4945dafb51569155635c91ca453b455dc31518b5
Dominique Liau <deejay.whitegrass@gmail.com>
2017-08-17 13:06:46 -0500

    move html from abouttokensale to abovethefold

--- src/views/Home/About-Token-Sale.vue
+++ src/views/Home/About-Token-Sale.vue
@@ -1,19 +1,6 @@
 <template>
   <section>
-    <b-jumbotron
-      class="bkg-color--purple"
-      header="A Decentralized Research Platform."
-      lead="Driven by Collaboration &amp; Competition." >
-    </b-jumbotron>
-    <div class="media-mentions bkg-purple">
-      <p>Featured in</p>
-      <div class="media-mentions__img-container">
-        <img src="../../assets/images/gizmodo.png" alt="">
-        <img src="../../assets/images/sdut-logo.png" alt="">
-        <img src="../../assets/images/UCSD-logo.png" alt="">
-        <img src="../../assets/images/singularity.png" alt="">
-      </div>
-    </div>
+    
   </section>
 </template>
 
