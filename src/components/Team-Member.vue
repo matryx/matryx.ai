c19863a0cd363101aaf0712220c046ec8153c070
Dominique Liau <deejay.whitegrass@gmail.com>
2017-08-17 14:32:43 -0500

    Add team members

--- src/components/Team-Member.vue
+++ src/components/Team-Member.vue
@@ -1,14 +1,59 @@
 <template>
-  <section></section>
+  <section class="team-member">
+    <img class="team-img" :src="src" :alt="alt">
+    <span class="name">{{ name }}</span>
+    <div class="title" {{ title }} </span>
+    <div class="social-icons">
+      <img class="linkedin"
+        src=""
+        alt="LinkedIn Icon"
+      >
+      <img class="twitter"
+        :src="twitterIcon"
+        alt="Twitter Icon"
+      >
+    </div>
+  </section>
 </template>
 
 <script>
+import twitterIcon from '../assets/icons/icon-twitter.gif'
+
 export default {
-  name: 'TeamMember'
+  name: 'TeamMember',
+
+  props: {
+    src: String,
+    alt: String,
+    name: String
+  },
+
+  data () {
+    return {
+      twitterIcon
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-@import '../../assets/styles';
+@import '../assets/css/styles';
+
+.team-member {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  width: 200px;
+}
+
+.team-img {
+  height: 120px;
+  width: 120px;
+  border-radius: 100%;
+}
+
+.name {
+}
 
 </style>
--- src/views/Home/Matryx-Team.vue
+++ src/views/Home/Matryx-Team.vue
@@ -1,14 +1,51 @@
 <template>
-  <section>matryx team</section>
+  <section>
+    <div class="content-container content-container--large">
+      <div class="team-title">
+        <h3>The Team</h3>
+      </div>
+
+      <div class="team-members" v-for="member in team">
+        <Team-Member
+          :src="member.src"
+          :alt="member.name"
+          :name="member.name">
+        </Team-Member>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import TeamMember from '@/components/Team-Member'
+import Steve from '@/assets/images/team/s-steven.jpg'
+import Keita from '@/assets/images/team/keita-1024x1024.jpg'
 export default {
-  name: 'MatryxTeam'
+  name: 'MatryxTeam',
+
+  components: {
+    TeamMember
+  },
+
+  data () {
+    return {
+      team: [{
+        name: 'Steve McCloskey',
+        title: 'Chief Executive Officer',
+        src: Steve
+      }, {
+        name: 'Keita Funakawa',
+        title: 'Chief Operations Officer',
+        src: Keita
+      }]
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 @import '../../assets/css/styles';
 
+.team-title {
+}
 </style>
