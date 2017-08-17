2895d8e74fe6c205fa78ab8e71c7f05222449e12
Dominique Liau <deejay.whitegrass@gmail.com>
2017-08-17 16:50:27 -0500

    Events

--- src/components/Primary-Event.vue
+++ src/components/Primary-Event.vue
@@ -1,10 +1,29 @@
 <template>
-  <section></section>
+  <div class="primary-event">
+    <div class="primary-event__heading">
+      <div class="primary-event__heading__name">{{ name }}</div>
+      <div class="primary-event__heading__date">{{ date }}</div>
+    </div>
+
+    <div class="primary-event__description">
+      {{ desc }}
+    </div>
+
+    <button class="btn-purple">
+      cta
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'PrimaryEvent'
+  name: 'PrimaryEvent',
+
+  props: {
+    name: String,
+    date: String,
+    desc: String
+  }
 }
 </script>
 
--- src/views/Home/News-Events.vue
+++ src/views/Home/News-Events.vue
@@ -1,13 +1,53 @@
 <template>
-  <section>news events</section>
+  <section class="news-events">
+    <div class="content-container content-container--large">
+      <div class="news-events__title">
+        <h2>News & Events</h2>
+        <div class="news-events__title__sub">News</div>
+        <div class="news-events__title__line">News</div>
+      </div>
+
+      <div class="news-events__events">
+        <div class="news-events__events__primary" v-for="event in events">
+          <Primary-Event
+            :name="event.name"
+            :date="event.date"
+            :desc="event.desc"
+          >
+          </Primary-Event>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import PrimaryEvent from '@/components/Primary-Event'
+
 export default {
-  name: 'NewsEvents'
+  name: 'NewsEvents',
+
+  components: {
+    PrimaryEvent
+  },
+
+  data () {
+    return {
+      events: [{
+        name: 'Big Event',
+        date: '10 Sept 2081',
+        description: 'asjdkfls;dalfkdsjfkl;alskdjfkdlsa'
+      }]
+    }
+  }
 }
 </script>
 
 <style lang="scss">
+@import '../../assets/css/colors';
+
+.news-events__title h2 {
+  color: $purple;
+}
 
 </style>
