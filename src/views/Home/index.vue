cfabace8593a00651d52f7a40056e39fb64e4358
Dominique Liau <deejay.whitegrass@gmail.com>
2017-08-17 10:30:15 -0500

    add featured in images

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -6,10 +6,13 @@
       lead="Driven by Collaboration &amp; Competition." >
     </b-jumbotron>
     <div class="media-mentions bkg-purple">
-      <img src="../../assets/images/gizmodo.svg" alt="">
-      <img src="../../assets/images/The_San_Diego_Union-Tribune.png" alt="">
-      <img src="../../assets/images/UCSanDiego-JacobsSchool-Black.png" alt="">
-      <img src="../../assets/images/logo-singularity-hub.svg" alt="">
+      <p>Featured in</p>
+      <div class="media-mentions__img-container">
+        <img src="../../assets/images/gizmodo.png" alt="">
+        <img src="../../assets/images/sdut-logo.png" alt="">
+        <img src="../../assets/images/UCSD-logo.png" alt="">
+        <img src="../../assets/images/singularity.png" alt="">
+      </div>
     </div>
   </div>
 </template>
