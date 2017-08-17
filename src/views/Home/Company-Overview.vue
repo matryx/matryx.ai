79ed39a26b28b93c3a19787a1f2d609d89eb3404
Dominique Liau <deejay.whitegrass@gmail.com>
2017-08-17 14:50:00 -0500

    overview

--- src/views/Home/Company-Overview.vue
+++ src/views/Home/Company-Overview.vue
@@ -1,8 +1,20 @@
 <template>
   <section>
     <div class="content-container--large content-container">
-      <h1 class="text-color--purple text--bold">Company Overview</h1>
-      
+      <h1 class="text-color--purple text--bold font-size--large">Company Overview</h1>
+      <div class="overview">
+        <div class="overview__column-1">
+          <h1>Company Background</h1>
+          <p class="text-color--light-purple">
+            Lorem ipsum dolor sit amet, integre prompta sadipscing et per.
+            No eius utinam fierent sea. Duo illud error at, has minim aliquid oportere in,
+            feugiat laboramus mel ei. Ipsum fugit cu eam, pri id omittam consulatu.
+             Sed cu eirmod commune deterruisset, ceteros maiorum qui ut.
+          </p>
+        </div>
+        <div class="overview__column-2"></div>
+      </div>
+
     </div>
   </section>
 </template>
