26f9736d8f26e87a05c824f0260c5c710dd44415
Dominique Liau <deejay.whitegrass@gmail.com>
2017-08-18 13:46:35 -0500

    clean up

--- src/components/CTA-Banner.vue
+++ src/components/CTA-Banner.vue
@@ -1,53 +1,28 @@
 <template lang="html">
-  <section :class="{ 'purple-bkg': purpleBkg }">
+  <section :class="{ 'purple-bkg': purpleBkg, 'white': white }" class="cta-banner">
     <div class="cta content-container content-container--large">
       <h4 class="cta__main">
         Join the Token Sale
       </h4>
-      <h6>Enter your email address to stay updated on the token sale launch</h6>
-      <b-form class="form-inline cta__form">
-        <b-form-input class="form-control cta__form__email"
-        size="lg"
-        v-model="email"
-        type="email"
-        placeholder="your@email.com"
-        >
-        </b-form-input>
-        <button class="btn btn-yellow cta__form__submit"
-        :class="{ 'purple-btn': purpleBkg }"
-        @click.prevent="getNotified"
-        >
-        Get Notified
-        </button>
-      </b-form>
+      <h6 class="cta__desc">Enter your email address to stay updated on the token sale launch</h6>
+      <Get-Notified :purpleBkg="purpleBkg" :ctaLocation="ctaLocation">
+      </Get-Notified>
     </div>
   </section>
 </template>
 
 <script>
-import { getUTMS } from '@/utils'
+import GetNotified from '@/components/Get-Notified'
 
 export default {
   name: 'CTABanner',
+
+  components: { GetNotified },
 
   props: {
     white: Boolean,
     purpleBkg: Boolean,
     ctaLocation: String
-  },
-
-  data () {
-    return {
-      email: ''
-    }
-  },
-
-  methods: {
-    getNotified () {
-      console.log('hello', this.ctaLocation, this.email)
-      console.log(getUTMS())
-      window.analytics.track(`CTA ${this.ctaLocation} Click`)
-    }
   }
 }
 </script>
@@ -57,63 +32,19 @@ export default {
 
 .cta {
   text-align: center;
-
-  &__form {
-    display: flex;
-    justify-content: center;
-  }
 }
 
-.purple-bkg {
-  background-color: $purple;
-  color: $white;
+.white {
+  color: white;
 }
 
-.purple-btn {
-  border: none;
+.cta__main {
+  font-size: 26px;
+  margin-bottom: 15px;
 }
 
-.cta .cta__form__email {
-  border-radius: 20px;
-  border-top-right-radius: 0;
-  border-bottom-right-radius: 0;
-  height: 42px;
-  border-right: none;
-  font-size: 12px;
-  width: 200px;
-
-  &:active, &:focus {
-    outline: none;
-  }
+.cta__desc {
+  font-size: 16px;
 }
 
-.cta .cta__form__submit {
-  border-radius: 20px;
-  border-top-left-radius: 0;
-  border-bottom-left-radius: 0;
-  height: 40px;
-}
-
-/*----- MEDIA QUERIES -----*/
-@media screen and (max-width: 500px) {
-  .cta .cta__form__email {
-    width: calc(100% - 20px);
-    border-top-right-radius: 40px;
-    border-bottom-right-radius: 40px;
-    border-top-left-radius: 40px;
-    border-bottom-left-radius: 40px;
-  }
-  .cta .cta__form__submit {
-    width: calc(100% - 140px);
-    margin-top: 10px;
-    border-top-right-radius: 40px;
-    border-bottom-right-radius: 40px;
-    border-top-left-radius: 40px;
-    border-bottom-left-radius: 40px;
-  }
-}
-
-#cta-bottom .cta.content-container{
-  padding-bottom: 0;
-}
 </style>
