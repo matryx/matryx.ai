758ef9a83cb052cc7aa96c3ab4a61012d80d7fb7
Dominique Liau <deejay.whitegrass@gmail.com>
2017-08-19 17:46:59 -0500

    add quote carousel

--- src/views/Home/Quotes.vue
+++ src/views/Home/Quotes.vue
@@ -3,44 +3,65 @@
     <div class="content-container content-container--small" >
       <h1 class="text-color--white">What People are Saying</h1>
 
-      <b-carousel id="quote-carousel" indicators :interval="3000">
+      <carousel
+      :pagination-enabled=true
+      :per-page=1
+      :scroll-per-page=true
+      :click-target-size=20
+      :autoplay=true
+      :autoplay-timeout="5000">
+       <slide>
+         Slide 1 Content
+       </slide>
+       <slide>
+         Slide 2 Content
+       </slide>
+     </carousel>
 
-      <b-carousel-slide>
-        <p class="quote-text text-color--white">
-          Mollit commodo in exercitation duis irure sit fugiat non consequat pariatur exercitation labore tempor nulla eu elit dolor.
-        </p>
-        <h3 class="quote-author text-color--white">
-          <span class="quote-author__name">John Smith</span>
-          <span class="quote-author__company">company name</span>
-        </h3>
-      </b-carousel-slide>
-      <b-carousel-slide >
-        <p class="quote-text text-color--white">
-          Proident aliqua exercitation incididunt nostrud irure eiusmod nulla aute pariatur.
-        </p>
-        <h3 class="quote-author text-color--white">
-          <span class="quote-author__name">John Smith</span>
-          <span class="quote-author__company">company name</span>
-        </h3>
-      </b-carousel-slide>
+      <!-- <b-carousel id="quote-carousel" indicators :interval="3000">
 
-    </b-carousel>
+        <b-carousel-slide>
+          <p class="quote-text text-color--white">
+            Mollit commodo in exercitation duis irure sit fugiat non consequat pariatur exercitation labore tempor nulla eu elit dolor.
+          </p>
+          <h3 class="quote-author text-color--white">
+            <span class="quote-author__name">John Smith</span>
+            <span class="quote-author__company">company name</span>
+          </h3>
+        </b-carousel-slide>
+        <b-carousel-slide >
+          <p class="quote-text text-color--white">
+            Proident aliqua exercitation incididunt nostrud irure eiusmod nulla aute pariatur.
+          </p>
+          <h3 class="quote-author text-color--white">
+            <span class="quote-author__name">John Smith</span>
+            <span class="quote-author__company">company name</span>
+          </h3>
+        </b-carousel-slide>
+
+      </b-carousel> -->
 
     </div>
   </section>
 </template>
 
 <script>
+import { Carousel, Slide } from 'vue-carousel'
+
 export default {
   name: 'Quotes',
-  methods: {
-    onSlide (slide) {
-      this.sliding = true
-    },
-    onSlid (slide) {
-      this.sliding = false
-    }
+  components: {
+    Carousel,
+    Slide
   }
+  // methods: {
+  //   onSlide (slide) {
+  //     this.sliding = true
+  //   },
+  //   onSlid (slide) {
+  //     this.sliding = false
+  //   }
+  // }
 }
 </script>
 
