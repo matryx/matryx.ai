f4ec52d59b9bd7bfa349abd54eb070a689a04457
Dominique Liau <deejay.whitegrass@gmail.com>
2017-08-20 09:57:30 -0500

    install jquery

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'jquery/dist/jquery.min.js'
+import 'jquery'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
