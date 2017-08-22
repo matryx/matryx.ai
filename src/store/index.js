687630dcc36f62bbadee9067c0306b969da79970
Dominique Liau <deejay.whitegrass@gmail.com>
2017-08-22 16:21:24 -0600

    start filling store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,42 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as actions from './actions'
-import * as getters from './getters'
 
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
-  actions,
-  getters,
+  actions: {
+    toggleModal ({ commit }, data) {
+      this.survey.open = true
+      this.survey.location = data.location
+    },
+    submitSurvey ({ commit }, data) {
+
+    }
+  },
+  getters: {
+
+  },
+  mutations: {
+    survey: {
+      submitted: false,
+      open: false,
+      location: ''
+    },
+    email: ''
+  }
   strict: debug
 })
+
+
+
+import * as types from './mutation-types'
+
+export const addToCart = ({ commit }, product) => {
+  if (product.inventory > 0) {
+    commit(types.ADD_TO_CART, {
+      id: product.id
+    })
+  }
+}
