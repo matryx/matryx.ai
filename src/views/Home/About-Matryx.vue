355f80fab0df7e6c9b9fe1ed68a3cf5767e981fb
Dominique Liau <deejay.whitegrass@gmail.com>
2017-08-27 17:52:12 -0600

    update matryx header

--- src/views/Home/About-Matryx.vue
+++ src/views/Home/About-Matryx.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <section class="content-container content-container--large ">
-    <h2 class="text--center"> Matryx: A Decentralized Collaboration Platform</h2>
+    <h1 class="text--center"> Matryx: A Decentralized Collaboration Platform</h1>
     <p class="text-color--matryx-dark-grey">Matryx is a platform for decentralized collaboration. Matryx consists of a bounty system, a library of digital assets, and a marketplace. Problems are posted, along with a bounty for a verified solution. Users then collaborate to solve problems, share results, and earn rewards. Rewards are given to all relevant contributors, and all submissions are added to the Matryx library and marketplace for future purchase. This will create an ecosystem of public collaboration and ideas that will drive research and innovation.</p>
   </section>
 </template>
@@ -17,6 +17,11 @@ export default {
 section.content-container {
   border-top: 1px solid $matryx-grey;
   border-bottom: 1px solid $matryx-grey;
-  padding:40px 80px ;
+  padding: 40px 80px;
+
+  h1 {
+    margin-bottom: 20px;
+    color: $matryx-black;
+  }
 }
 </style>
