995c69e3d576e76262d6305d35adb093013455fd
Dominique Liau <deejay.whitegrass@gmail.com>
2017-08-28 09:46:29 -0600

    add media queries and update font size

--- src/views/Home/About-Matryx.vue
+++ src/views/Home/About-Matryx.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <section class="content-container content-container--large ">
-    <h1 class="text--center"> Matryx: A Decentralized Collaboration Platform</h1>
+    <h1 class="text--center text-color--matryx-dark-grey"> Matryx: A Decentralized Collaboration Platform</h1>
     <p class="text-color--matryx-dark-grey">Matryx is a platform for decentralized collaboration. Matryx consists of a bounty system, a library of digital assets, and a marketplace. Problems are posted, along with a bounty for a verified solution. Users then collaborate to solve problems, share results, and earn rewards. Rewards are given to all relevant contributors, and all submissions are added to the Matryx library and marketplace for future purchase. This will create an ecosystem of public collaboration and ideas that will drive research and innovation.</p>
   </section>
 </template>
@@ -20,8 +20,14 @@ section.content-container {
   padding: 40px 80px;
 
   h1 {
+    font-size: 46px;
     margin-bottom: 20px;
-    color: $matryx-black;
+  }
+
+  @media screen and (max-width: 850px) {
+    section.content-container {
+      padding: 40px;
+    }
   }
 }
 </style>
