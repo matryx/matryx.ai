fe97978af92c914a5dcf8504c7500c77de7010f4
Dominique Liau <deejay.whitegrass@gmail.com>
2017-08-31 20:01:22 -0600

    move media mentions

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,31 +3,7 @@
     <Navbar-Token></Navbar-Token>
     <Navbar-Sticky></Navbar-Sticky>
     <Above-the-Fold></Above-the-Fold>
-    <div class="media-mentions bkg-color--white">
-      <div class=" content-container content-container--medium media-mentions__img-container">
-        <p class="text-color--matryx-grey">
-          <span v-if=" language === 'ru' ">Было показано в</span>
-          <span v-else-if=" language === 'ch' ">刊登在</span>
-          <span v-else-if=" language === 'ge' ">Vorgestellt in</span>
-          <span v-else-if=" language === 'ja' ">以下のメディアで取り上げられました:</span>
-          <span v-else-if=" language === 'ko' ">다뤄진 매체들</span>
-          <span v-else>Featured in</span>
-        </p>
-        <a class="press-logo" href="https://www.gizmodo.com.au/2017/01/you-can-do-maths-in-virtual-reality-now/" target="_blank">
-          <img src="../../assets/images/press/gizmodo.png" alt=""></a>
-        <a class="press-logo" href="http://markets.businessinsider.com/news/stocks/Virtual-Reality-Software-Maker-Nanome-Introduces-Matryx-with-Token-Sale-1002282833?utm_source=matryx&utm_medium=web" target="_blank">
-          <img src="../../assets/images/press/business-insider.png" alt=""></a>
-        <a class="press-logo" href="http://www.sandiegouniontribune.com/business/sd-fi-ignite-conference-20170223-story.html" target="_blank">
-          <img src="../../assets/images/press/sdut-logo.png" alt=""></a>
-
-        <a class="press-logo" href="http://jacobsschool.ucsd.edu/news/news_releases/release.sfe?id=2081" target="_blank">
-          <img src="../../assets/images/press/UCSD-logo.png" alt=""></a>
-        <a class="press-logo" href="http://sdtimes.com/nanome-launches-vr-collaboration-platform-matryx/" target="_blank">
-          <img src="../../assets/images/press/sd_times.png" alt=""></a>
-        <a class="press-logo" href="https://singularityhub.com/2017/01/18/get-ready-to-love-math-with-this-sweet-vr-calculator/" target="_blank">
-          <img src="../../assets/images/press/singularity.png" alt=""></a>
-      </div>
-    </div>
+    <Media-Mentions></Media-Mentions>
     <About-Matryx></About-Matryx>
     <Pre-Sale></Pre-Sale>
     <div class="cta-middle-banner">
@@ -57,6 +33,7 @@ import AboveTheFold from './Above-the-Fold'
 import BountySystem from './Bounty-System'
 import CompanyOverview from './Company-Overview'
 import MatryxTeam from './Matryx-Team'
+import MediaMentions from './Media-Mentions'
 import NanomeInfo from './Nanome-Info'
 import NavbarSticky from './Navbar-Sticky'
 import NewsEvents from './News-Events'
@@ -84,6 +61,7 @@ export default {
     BountySystem,
     CompanyOverview,
     MatryxTeam,
+    MediaMentions,
     NanomeInfo,
     NavbarSticky,
     NewsEvents,
@@ -122,44 +100,4 @@ export default {
 .content-container{
   padding: 0;
 }
-.media-mentions {
-  display:-webkit-box;display:-ms-flexbox;display:flex;
-  flex-direction: column;
-
-  p{
-    text-align: center;
-    text-transform: uppercase;
-    width: 100%;
-  }
-  a{
-    opacity: 0.2;
-    padding: 25px;
-  }
-  a:hover{
-    opacity: 1;
-    transition: all 0.3s;
-  }
-
-  &__img-container {
-    display:-webkit-box;display:-ms-flexbox;display:flex;
-    align-items: center;
-    justify-content: center;
-    flex-wrap: wrap;
-
-    img {
-      max-width: 200px;
-      padding: 10px;
-    }
-  }
-}
-
-@media screen and (max-width: 500px) {
-  .media-mentions__img-container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-wrap: wrap;
-  }
-}
-
 </style>
