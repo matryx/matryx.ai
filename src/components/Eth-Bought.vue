90b68c5da7e77779abbdd71b927d22ee39ef25e7
Dominique Liau <deejay.whitegrass@gmail.com>
2017-09-21 15:44:27 -0500

    add eth counter

--- src/components/Eth-Bought.vue
+++ src/components/Eth-Bought.vue
@@ -1,17 +1,57 @@
 <template lang="html">
   <div class="eth-bought__container">
     <img src="../assets/icons/icon-ethereum.png" alt="" class="eth-bought__logo">
-    <p class="eth-bought__value">{{ ethValue }} Contributed</p>
+    <p class="eth-bought__value">{{ counter }} Contributed</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import config from '../../config'
+
 export default {
   name: 'EthBought',
-  props: {
-    ethValue: {
-      type: Number,
-      default: 2600
+  // props: {
+  //   ethValue: {
+  //     type: Number,
+  //     default: 2600
+  //   },
+  //   current: Number
+  // },
+  data () {
+    return {
+      counter: 0,
+      current: Number
+    }
+  },
+
+  mounted () {
+    axios.get(`${config.app.host}/api/sold`)
+      .then((result) => {
+        this.counter = Math.floor(result.data.total - 50)
+        this.current = Math.floor(result.data.total)
+
+        setTimeout(() => {
+          const counter = setInterval(() => {
+            this.counter++
+            if (this.counter >= this.current) {
+              clearCounter()
+            }
+          }, 100)
+
+          function clearCounter () {
+            clearInterval(counter)
+          }
+        }, 500)
+      })
+      .catch((err) => {
+        console.log('Error retrieving total sold', err)
+      })
+  },
+
+  computed: {
+    ETHProgress () {
+      return `width: ${this.ETHPercent}%;`
     }
   }
 }
